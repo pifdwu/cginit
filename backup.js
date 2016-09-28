1f5a88b45f2b4d6bc483bbde035b28e9ba6bd184
pifdwu <pifdwu@163.com>
2016-09-28 21:36:19 +0800

    Create backup.js

---
+++ backup.js
@@ -0,0 +1,11 @@
+{
+"scheme":"https",
+"domain":"user.auth204.xyz:21568",
+"ip":"119.28.18.81:80",
+"show_error_notify":false,
+"show_init_notify":false,
+"ipaccess":true,
+"reg_login_url":"user.auth203.xyz:22135",
+"githuburl":"https://github.com/chuguofan/chuguo",
+"siteurl":"https://a.tzqby.xyz:17869"
+}
