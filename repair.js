0e9a9ce7a053a67aae97c448c41a451d0441b44a
pifdwu <pifdwu@163.com>
2016-09-29 15:57:17 +0800

    Update repair.js

--- repair.js
+++ repair.js
@@ -4,7 +4,7 @@
 "ip":"119.28.18.81:80",
 "show_error_notify":false,
 "show_init_notify":false,
-"ipaccess":true,
+"ipaccess":false,
 "reg_login_url":"user.auth203.xyz:22135",
 "githuburl":"https://github.com/chuguofan/chuguo",
 "siteurl":"https://a.tzqby.xyz:17869"
