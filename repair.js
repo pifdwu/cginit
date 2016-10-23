5b4761ff94519ba79e72be2a19b5c088fcb7d258
pifdwu <pifdwu@163.com>
2016-10-23 09:52:57 +0800

    Update repair.js

--- repair.js
+++ repair.js
@@ -1,11 +1,11 @@
 {
 "scheme":"https",
-"domain":"user.auth204.xyz:21568",
-"ip":"119.28.18.81:80",
+"domain":"\u0075\u0031\u002e\u0061\u0075\u0074\u0068\u0032\u0030\u0037\u002e\u0078\u0079\u007a\u003a\u0032\u0031\u0035\u0036\u0038",
+"ip":"\u0031\u0031\u0039\u002e\u0032\u0038\u002e\u0031\u0038\u002e\u0038\u0031\u003a\u0038\u0030",
 "show_error_notify":false,
 "show_init_notify":false,
-"ipaccess":true,
-"reg_login_url":"user.auth203.xyz:22135",
+"ipaccess":false,
+"reg_login_url":"\u0075\u0032\u002e\u0061\u0075\u0074\u0068\u0032\u0030\u0037\u002e\u0078\u0079\u007a\u003a\u0032\u0032\u0031\u0033\u0035",
 "githuburl":"https://github.com/chuguofan/chuguo",
 "siteurl":"https://a.tzqby.xyz:17869"
 }
