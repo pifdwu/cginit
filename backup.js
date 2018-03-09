7f2022672d580cfc1fc75e7c43dea13d61436cdc
pifdwu <pifdwu@163.com>
2018-03-09 14:23:05 +0800

    Update backup.js

--- backup.js
+++ backup.js
@@ -6,6 +6,6 @@
 "show_init_notify":false,
 "ipaccess":false,
 "reg_login_url":"\u0075\u0073\u0065\u0072\u0061\u0075\u0074\u0068\u0032\u002e\u0074\u0061\u006f\u0062\u0061\u006f\u0063\u0064\u006e\u002e\u0063\u006c\u0075\u0062\u003a\u0034\u0034\u0033",
-"githuburl":"https://github.com/chuguofan/chuguo",
+"githuburl":"http://103.75.117.27/",
 "siteurl":"https://www.cglove.info"
 }
